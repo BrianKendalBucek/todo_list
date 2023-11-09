6f003b4f82f7d02f7aa1b937bef0e73d56c232d7
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-11-08 16:09:47 -0800

    managed to get the dates also

--- walmart_scraper/spiders/article.py
+++ walmart_scraper/spiders/article.py
@@ -5,18 +5,22 @@ class CostaRicaBlogSpider(scrapy.Spider):
     start_urls = ['https://www.liveincostarica.com/blog/articles']
 
     custom_settings = {
-    'FEEDS': {
-        'data/%(name)s_%(time)s.json': {
-            'format': 'json',
-            'fields': ['title', 'paragraphs', 'url'],
+        'FEEDS': {
+            'data/%(name)s_%(time)s.json': {
+                'format': 'json',
+                'fields': ['date', 'title', 'paragraphs', 'url'],
+            },
         },
-    },
-}
+    }
 
     def parse(self, response):
-        # Find links to each article and iterate over them
-        for article in response.css('.entry-title a::attr(href)').getall():
-            yield response.follow(article, self.parse_article)
+        # Find each article entry, extract the date, and follow the link to the article
+        for article in response.css('.no-post-author'):
+            article_url = article.css('.entry-title a::attr(href)').get()
+            article_date = article.css('time.post-date::attr(datetime)').get()
+
+            # Pass the extracted date to the parse_article method
+            yield response.follow(article_url, self.parse_article, meta={'article_date': article_date})
 
         # Find the link to the next page and go there
         next_page = response.css('a.number.nextp::attr(href)').get()
@@ -28,7 +32,11 @@ class CostaRicaBlogSpider(scrapy.Spider):
         title = response.css('h1 a::text').get()
         paragraphs = response.css('.entry-content p::text').getall()
 
+        # Retrieve the date passed from the parse method
+        date = response.meta['article_date']
+
         yield {
+            'date': date,
             'title': title,
             'paragraphs': paragraphs,
             'url': response.url
